b82ed921e9347bb6ddcc2f5f5f202c8a1c4a71da
Welder n. Fernandes <welderx3@gmail.com>
2024-03-19 16:48:45 -0300

    fix: eslint to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['https://github.com', 'avatars.githubusercontent.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ['./components/ui/*'],
+  },
 }
 
 export default nextConfig
