099b68129fdb76a949406347432b9b0224cb6570
Welder Nascimento Fernandes <welderx3@gmail.com>
2024-05-07 10:53:25 -0300

    refactor: ajusted avatar

--- src/app/(application)/establishment/[id]/layout.tsx
+++ src/app/(application)/establishment/[id]/layout.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/src/components/ui/button'
 import { ReactNode } from 'react'
 import { FaArrowLeftLong } from 'react-icons/fa6'
 import { Header } from '../../_components/header'
-import { ItemAvatar } from '../../_components/item/item-avatar'
 
 export default function EstablishmentLayout({
   children,
@@ -24,14 +23,6 @@ export default function EstablishmentLayout({
         <Header.Content>
           <Header.Title className="text-neutral-900" title="Agendar Consulta" />
         </Header.Content>
-        <Button
-          variant={'link'}
-          onClick={() => {
-            alert('teste')
-          }}
-        >
-          <ItemAvatar imageUrl="https://github.com/welde.png" alt="welde" />
-        </Button>
       </Header.Root>
       {children}
     </div>
